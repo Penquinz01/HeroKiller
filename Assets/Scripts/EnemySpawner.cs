dba414334e30a6fe5c0fe29cbc9935546525c672
Antony Sajan <antonysajant@gmail.com>
2025-12-09 21:46:12 +0530

    spawn cooldown

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -4,22 +4,36 @@ public class EnemySpawner : MonoBehaviour
 {
     Vector2 heroPos;
     [SerializeField] GameObject enemy;
+    [SerializeField] int enemyCount = 3;
+    [SerializeField] float cooldown = 3f;
+    [SerializeField] float nextCool;
+    [SerializeField] float timer;
+
     void Start()
     {
         heroPos = GameObject.FindGameObjectWithTag("Hero").transform.position;
+        nextCool = 0f;
     }
     void Update()
     {
+        timer=Time.time;
         heroPos = GameObject.FindGameObjectWithTag("Hero").transform.position;
         if (Input.GetKeyDown(KeyCode.Alpha1))
         {
-            int ch = Random.Range(1, 5); // 1:left, 2:top, 3:right, 4:bottom
-            switch (ch) 
+            if (Time.time >= nextCool)
             {
-                case 1: Instantiate(enemy, new Vector2(heroPos.x - 7f, heroPos.y + Random.Range(-4f, 4f)), Quaternion.identity); break; //left
-                case 2: Instantiate(enemy, new Vector2(heroPos.x + Random.Range(-7f,7f), heroPos.y + 4f), Quaternion.identity); break;  //top
-                case 3: Instantiate(enemy, new Vector2(heroPos.x + 7f, heroPos.y + Random.Range(-4f, 4f)), Quaternion.identity); break; //right
-                case 4: Instantiate(enemy, new Vector2(heroPos.x + Random.Range(-7f, 7f), heroPos.y - 4f), Quaternion.identity); break; //bottom
+                for (int i = 0; i < enemyCount; i++)
+                {
+                    int ch = Random.Range(1, 5); // 1:left, 2:top, 3:right, 4:bottom
+                    switch (ch)
+                    {
+                        case 1: Instantiate(enemy, new Vector2(heroPos.x - 7f, heroPos.y + Random.Range(-4f, 4f)), Quaternion.identity); break; //left
+                        case 2: Instantiate(enemy, new Vector2(heroPos.x + Random.Range(-7f, 7f), heroPos.y + 4f), Quaternion.identity); break;  //top
+                        case 3: Instantiate(enemy, new Vector2(heroPos.x + 7f, heroPos.y + Random.Range(-4f, 4f)), Quaternion.identity); break; //right
+                        case 4: Instantiate(enemy, new Vector2(heroPos.x + Random.Range(-7f, 7f), heroPos.y - 4f), Quaternion.identity); break; //bottom
+                    }
+                }
+                nextCool = Time.time + cooldown;
             }
         }
     }
